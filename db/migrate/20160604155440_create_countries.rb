12fbae05251b0184137157dd643b9e08b82c5d28
Hank Chen <email": null>
2016-06-04 15:57:57 -0700

    create countri model and migration

---
+++ db/migrate/20160604155440_create_countries.rb
@@ -0,0 +1,10 @@
+class CreateCountries < ActiveRecord::Migration
+  def change
+    creat_table :countries do |t|
+      t.string :country_name
+      t.integer :image_id
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,20 +9,6 @@
   )
 end
 
-100.times do (
-  Following.create(
-    follower_id: rand(1..20),
-    followee_id: rand(1..20)
-    )
-  )
-end
-
-36.times do (
-  Tweet.create(
-    body: Faker::Hipster.sentence,
-    user_id: rand(1..20)
-    )
-  )
 end
 
 1000.times do (
@@ -33,37 +19,10 @@ end
 end
 
 User.create(
-  first_name: Brian,
-    last_name: Chung,
-    user_name: bc123,
-    image:
-    password: "123"
-)
-User.create(
   first_name: Hank,
     last_name: Chen,
     user_name: hc123
     image:
     password: "123"
 )
-User.create(
-  first_name: Shin,
-    last_name: Wang,
-    user_name: sw123
-    image:
-    password: "123"
-)
-User.create(
-  first_name: Chris,
-    last_name: Lamkins,
-    user_name: cl123
-    image:
-    password: "123"
-)
-User.create(
-  first_name: John,
-    last_name: M,
-    user_name: jm123
-    image:
-    password: "123"
-)
+

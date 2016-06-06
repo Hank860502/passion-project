628cd7fe7ee7680c42ffa3e3d2a72d03ae571f26
Hank860502 <hank860502@gmail.com>
2016-06-06 14:46:57 -0700

    move to desktop

--- public/js/init.js
+++ public/js/init.js
@@ -2,4 +2,7 @@ $(document).ready(function() {
 Materialize.updateTextFields();
 $('select').material_select();
 $('input#input_text, textarea#textarea1').characterCounter();
+// var product = moltin.Product.Find({slug: 'decorative-hedgehogs'});
+// console.log(product)
+// debugger;
 });

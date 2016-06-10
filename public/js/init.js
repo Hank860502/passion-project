014431c199a5c9b3db869d580db150c53a3b7f76
Hank860502 <hank860502@gmail.com>
2016-06-09 19:38:08 -0700

    add animation when hover through icon

--- public/js/init.js
+++ public/js/init.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
-Materialize.updateTextFields();
-$('select').material_select();
-$('input#input_text, textarea#textarea1').characterCounter();
+  Materialize.updateTextFields();
+  $('select').material_select();
+  $('input#input_text, textarea#textarea1').characterCounter();
   var moltin = new Moltin({publicId: 'NGtMpNEgNZhfU9MHjFAfDja1CkkhoiRvkg6JUkCS3A'});
   moltin.Authenticate(function() {
 
 
-$(".add_to_wishlist").on("click", function(e) {
-  e.preventDefault()
- item_id = $(this).attr("id")
- item_name = $(this).attr("data")
- console.log(item_name)
+    $(".add_to_wishlist").on("click", function(e) {
+      e.preventDefault()
+      item_id = $(this).attr("id")
+      item_name = $(this).attr("data")
+      console.log(item_name)
 // var promise = new Promise(function(fulfill, reject) {
   var product = moltin.Product.Find({slug: item_name});
   console.log(product[0].id)
@@ -19,25 +19,54 @@ $(".add_to_wishlist").on("click", function(e) {
 
   // })
   var item = moltin.Cart.Insert(product[0].id, 1, null);
-    console.log(item)
+  console.log(item)
   var cart = moltin.Cart.Contents();
   console.log(cart)
   $("wishlist").append(cart)
 })
-});
-
-$("#map").on("click", function(e) {
-  e.preventDefault()
-  console.log("box")
-  $(".tour").append("<div id='googleMap' style='width:500px;height:380px;'></div>")
-})
-
-  //   $(".products").html(product[0].description)
-  //   console.log(product)
   });
 
+  $("#map").on("click", function(e) {
+    e.preventDefault()
+    console.log("box")
+    $(".tour").append("<div id='googleMap' style='width:500px;height:380px;'></div>")
+  })
+  eventListeners();
+  // searchBar();
+  //   $(".products").html(product[0].description)
+  //   console.log(product)
+});
 
+var eventListeners = function(){
+  $("#button").on("click", searchBar);
+  $("#exit").on("click", hidesearchBar)
+  $("#taiwanbutton").hover(bigTaiwan, smallTaiwan)
+}
 
+function searchBar(e){
+  e.preventDefault();
+  $("#searchform").show();
+  $("#button").hide();
+  $("#taiwan").hide();
+  $("#uk").hide();
+}
+
+function hidesearchBar(e) {
+  e.preventDefault();
+  $("#searchform").hide();
+  $("#button").show();
+  $("#taiwan").show();
+  $("#uk").show();
+}
+
+function bigTaiwan(){
+  $("#taiwanbutton").removeClass("fa fa-map-marker fa-3x");
+  $("#taiwanbutton").addClass("fa fa-map-marker fa-4x");
+}
+function smallTaiwan(){
+  $(this).removeClass("fa fa-map-marker fa-4x")
+  $(this).addClass("fa fa-map-marker fa-3x")
+};
 
 
     // for (var i = products.length - 1; i >= 0; i--) {
